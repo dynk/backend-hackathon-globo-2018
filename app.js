b4fe990ff9072d6b5aa943d54905a5e0737bbb74
Raphael Salvador <raphaelsalvador@Raphaels-MacBook-Pro.local>
2018-04-28 23:41:02 -0300

    inserted process env port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const logger =  require('./utils/logger');
 const bodyParser = require('body-parser');
 
-const PORT = 3020;
+const LOCAL_PORT = 3020;
 
 // Logging unhandled promises rejection
 process.on('unhandledRejection', (reason, p) => {
@@ -16,9 +16,9 @@ app.use(bodyParser.json());
 
 app.use(`/`, require('./routes'));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || LOCAL_PORT, () => {
 
     console.log("jere");
-  logger.debug(`Listening on port ${PORT}`);
+  logger.debug(`Listening on port ${LOCAL_PORT}`);
 });
 
